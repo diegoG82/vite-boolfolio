167bd5b0e35c8d859abe4b9b9b1877d3b24b22eb
diegoG82 <diegogiardina@hotmail.it>
2023-06-13 16:32:22 +0200

    printed cards

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,44 @@
 <script>
-export default{
-  data(){
-    return{
-      projects:[]
-    }
+import axios from 'axios';
+import ProjectCard from './components/ProjectCard.vue';
+
+export default {
+  data() {
+    return {
+      baseUrl: "http://127.0.0.1:8000",
+      projects: []
+    };
   },
-  mounted(){
+  mounted() {
     this.getProjects();
   },
-  methods:{
-    getProjects(){
-console.log('test');
+  methods: {
+    getProjects() {
+      axios.get(`${this.baseUrl}/api/projects`)
+        .then(resp => {
+          this.projects = resp.data.results;
+        });
     }
-
-   
-  }
-}
-
+  },
+  components: { ProjectCard }
+};
 </script>
-<template>
-  <h1>Ciao</h1>
-</template>
+
+
+  <template>
+    <div class="container">
+      <h1 class="text-center mt-4 mb-4">PROJECT LIST</h1>
+      <div class="row row-cols-5 g-3">
+        <div class="col" v-for="project in projects" :key="project.id">
+          <ProjectCard :project="project" />
+        </div>
+      </div>
+    </div>
+  </template>
+
+
+
 
 <style lang="scss">
-@use "./styles/general.scss" as *
-
+@import "./styles/general.scss";
 </style>
---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,33 @@
+<script>
+export default {
+    name: 'ProjectCard',
+    props: {
+        project: Object
+    },
+    data() {
+        return {
+            baseUrl: "http://127.0.0.1:8000",
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="card h-100">
+        <h5 >{{ project.title }}</h5>
+        <img v-if="project.image" :src="`${baseUrl}/storage/${project.image}`" class="card-img-top" :alt="project.title" />
+        <div v-else>
+            Nessuna immagine presente
+        </div>
+        <div class="card-body">
+            <h5>{{ project.title }}</h5>
+            <p>{{ project.content }}</p>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+
+
+
+</style>
