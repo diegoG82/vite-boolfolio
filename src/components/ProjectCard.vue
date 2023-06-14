ca4be748ac49d2f1bbe02a42134cc30e4d03ee0e
diegoG82 <diegogiardina@hotmail.it>
2023-06-14 12:50:06 +0200

    done bonus 2

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -14,7 +14,7 @@ export default {
 
 <template>
     <div class="card h-100">
-        <h5 >{{ project.title }}</h5>
+        <h5>{{ project.title }}</h5>
         <img v-if="project.image" :src="`${baseUrl}/storage/${project.image}`" class="card-img-top" :alt="project.title" />
         <div v-else>
             Nessuna immagine presente
@@ -22,12 +22,18 @@ export default {
         <div class="card-body">
             <h5>{{ project.title }}</h5>
             <p>{{ project.content }}</p>
+            <p class="text-warning">PROJECT TYPE: {{ project.type.name }}</p>
+            <p class="text-success">
+                PROJECT TECH:
+                <span v-if="project.technologies && project.technologies.length > 0">
+                    {{ project.technologies.map(tech => tech.name).join(", ") }}
+                </span>
+                <span v-else>
+                    No tech
+                </span>
+            </p>
         </div>
     </div>
 </template>
 
-<style lang="scss" scoped>
-
-
-
-</style>
+<style lang="scss" scoped></style>
