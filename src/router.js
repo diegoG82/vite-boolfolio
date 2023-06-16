3bcfc83d1f7cf2bf577fb2fa3de78e316947396a
diegoG82 <diegogiardina@hotmail.it>
2023-06-16 16:12:14 +0200

    added lazy routes

--- src/router.js
+++ src/router.js
@@ -1,9 +1,49 @@
+// import { createRouter, createWebHistory } from "vue-router";
+// import Homepage from "./pages/HomePage.vue";
+// import ProjectPage from "./pages/ProjectPage.vue";
+// import AboutPage from "./pages/AboutPage.vue"
+// import SingleProjectPage from "./pages/SingleProjectPage.vue"
+// import NotFound from "./pages/NotFound.vue"
+
+// const router = createRouter({
+//   history: createWebHistory(),
+//   routes: [
+//     {
+//       path: "/",
+//       name: "home",
+//       component: Homepage,
+//     },
+//     {
+//       path: "/projects",
+//       name: "projects",
+//       component: ProjectPage,
+//     },
+
+//     {
+//       path: "/projects/:slug",
+//       name: "single-project",
+//       component: SingleProjectPage,
+//     },
+
+//     {
+//         path: "/aboutus",
+//         name: "aboutus",
+//         component: AboutPage,
+//       },
+
+//       {
+//         path: "/:pathMatch(.*)*",
+//         name: "not-found",
+//         component: NotFound,
+//       },
+//   ],
+// });
+
+// export { router };
+
+// LAZYLOADED ROUTES:
+
 import { createRouter, createWebHistory } from "vue-router";
-import Homepage from "./pages/HomePage.vue";
-import ProjectPage from "./pages/ProjectPage.vue";
-import AboutPage from "./pages/AboutPage.vue"
-import SingleProjectPage from "./pages/SingleProjectPage.vue"
-import NotFound from "./pages/NotFound.vue"
 
 const router = createRouter({
   history: createWebHistory(),
@@ -11,31 +51,28 @@ const router = createRouter({
     {
       path: "/",
       name: "home",
-      component: Homepage,
+      component: () => import("./pages/HomePage.vue"),
     },
     {
       path: "/projects",
       name: "projects",
-      component: ProjectPage,
+      component: () => import("./pages/ProjectPage.vue"),
     },
-
     {
       path: "/projects/:slug",
       name: "single-project",
-      component: SingleProjectPage,
+      component: () => import("./pages/SingleProjectPage.vue"),
     },
-
     {
-        path: "/aboutus",
-        name: "aboutus",
-        component: AboutPage,
-      },
-
-      {
-        path: "/:pathMatch(.*)*",
-        name: "not-found",
-        component: NotFound,
-      },
+      path: "/aboutus",
+      name: "aboutus",
+      component: () => import("./pages/AboutPage.vue"),
+    },
+    {
+      path: "/:pathMatch(.*)*",
+      name: "not-found",
+      component: () => import("./pages/NotFound.vue"),
+    },
   ],
 });
 
