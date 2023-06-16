3987f2a1e6d5b86deb96e1dc68d156d01772420a
diegoG82 <diegogiardina@hotmail.it>
2023-06-16 16:44:30 +0200

    added working toggle for navbar

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,4 +1,4 @@
-<script>
+<!-- <script>
 
 export default {
     name: "AppHeader",
@@ -51,4 +51,83 @@ export default {
 }
 
 
-</style>
+</style> -->
+
+<!-- CON TOGGLER ATTIVO -->
+<script>
+  export default {
+    name: "AppHeader",
+    data() {
+      return {
+        menuItems: [
+          {
+            label: "Home",
+            routeName: "home",
+          },
+          {
+            label: "Projects",
+            routeName: "projects",
+          },
+          {
+            label: "About Us",
+            routeName: "aboutus",
+          },
+        ],
+        isNavOpen: false,
+      };
+    },
+  };
+  </script>
+
+<template>
+    <header>
+      <nav class="navbar navbar-expand-lg bg-primary navbar-dark header">
+        <div class="container-fluid">
+          <div class="d-flex align-items-center">
+            <a class="navbar-brand text-dark" href="#">BoolFolio By Diego G.</a>
+            <div class="collapse navbar-collapse" :class="{ show: isNavOpen }" id="navbarNav">
+              <ul class="navbar-nav ms-auto">
+                <li class="nav-item" v-for="item in menuItems" :key="item.label">
+                  <router-link :to="{ name: item.routeName }" class="nav-link active text-dark" aria-current="page">
+                    {{ item.label }}
+                  </router-link>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+            @click="isNavOpen = !isNavOpen"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+        </div>
+      </nav>
+    </header>
+  </template>
+
+  
+  
+  
+  <style lang="scss" scoped>
+  .header {
+    height: 50px;
+  }
+  
+  .navbar-collapse.show {
+    background-color: var(--bs-primary);
+    padding: 10px;
+    position: absolute;
+    top: 100%;
+    left: 0;
+    width: 100%;
+    z-index: 9999;
+  }
+  </style>
+  
